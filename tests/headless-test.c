a5a328a90828b0d02ec19725d35fc6cf83f72c46
Derek Foreman <derekf@osg.samsung.com>
2014-11-06 10:20:21 -0600

    tests: add test of headless compositor screenshot mechanism

---
+++ tests/headless-test.c
@@ -0,0 +1,42 @@
+/*
+ * Copyright © 2014 Samsung
+ *
+ * Permission to use, copy, modify, distribute, and sell this software and
+ * its documentation for any purpose is hereby granted without fee, provided
+ * that the above copyright notice appear in all copies and that both that
+ * copyright notice and this permission notice appear in supporting
+ * documentation, and that the name of the copyright holders not be used in
+ * advertising or publicity pertaining to distribution of the software
+ * without specific, written prior permission.  The copyright holders make
+ * no representations about the suitability of this software for any
+ * purpose.  It is provided "as is" without express or implied warranty.
+ *
+ * THE COPYRIGHT HOLDERS DISCLAIM ALL WARRANTIES WITH REGARD TO THIS
+ * SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS, IN NO EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
+ * CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <unistd.h>
+
+#include "weston-test-client-helper.h"
+
+char *server_parameters="--use-pixman";
+
+TEST(headless)
+{
+	struct client *client;
+
+	client = client_create(100, 100, 100, 100);
+	assert(client);
+
+	sleep(5);
+
+	wl_test_record_screenshot(client->test->wl_test, "headless_screenshot");
+
+	client_roundtrip(client);
+
+}
