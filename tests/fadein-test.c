baa121ada22bf28fed95486b0007481838dd4333
Bryce Harrington <bryce@osg.samsung.com>
2014-12-19 17:45:43 -0800

    squash to fadein test: Implement screenshot saving clientside in the test
    This is WIP towards enabling screenshot saving in the client tests.

--- tests/fadein-test.c
+++ tests/fadein-test.c
@@ -24,11 +24,28 @@
 
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h> // memcpy
+#include <cairo.h>
 
 #include "weston-test-client-helper.h"
 
 char *server_parameters="--use-pixman --width=320 --height=240";
 int buffer_copy_done;
+
+// TODO: Isn't this already available from a header?
+static void *
+xmalloc(size_t size)
+{
+        void *p;
+
+        p = malloc(size);
+	if (p == NULL) {
+                fprintf(stderr, "out of memory\n");
+                exit(EXIT_FAILURE);
+        }
+
+        return p;
+}
 
 /*
  * Does the test infrastructure take care of hooking this up for us?
@@ -53,33 +70,78 @@ TEST(fadein)
 	bool match = false;
 	bool dump_all_images = true;
 
+	/* Create the client */
 	client = client_create(100, 100, 100, 100);
 	assert(client);
+	printf("Client created\n");
 
-	//  Create screenshot and attach buffer
+	/* Create a surface to hold the screenshot */
+	screenshot = xzalloc(sizeof *screenshot);
+	screenshot->wl_surface = wl_compositor_create_surface(client->wl_compositor);
+	assert(screenshot->wl_surface);
+	printf("Screenshot surface created\n");
+
+	/* Create and attach buffer to our surface */
 	screenshot->wl_buffer = create_shm_buffer(client,
 						  client->output->width,
 						  client->output->height,
 						  &screenshot->data);
+	assert(screenshot->wl_buffer);
+	printf("Screenshot buffer created and attached to surface\n");
 
 	usleep(750000);
 
+	/* Take a snapshot.  Result will be in screenshot->wl_buffer. */
 	wl_test_capture_screenshot(client->test->wl_test,
 				   client->output->wl_output,
 				   screenshot->wl_buffer);
-	while (!buffer_copy_done)
-		client_roundtrip(client);
+	printf("Capture request sent\n");
+	client_roundtrip(client);
+	printf("Roundtrip done\n");
 
-	// TODO: Receive screenshot from event
+	// TODO: Wait for the done event instead of just sleeping...
+	usleep(1000000);
+
 	clip.x = 0;
 	clip.y = 0;
 	clip.width = client->output->width;
 	clip.height = client->output->height;
 
+	printf("Clip: %d,%d %d x %d\n", clip.x, clip.y, clip.width, clip.height);
+
 	match = check_match(screenshot, &clip, reference);
 	if (!match || dump_all_images) {
-		// TODO: Write image to .png file
-	}
-	assert(match);
+		/* Write image to .png file */
+		int output_stride, buffer_stride, i;
+		cairo_surface_t *surface;
+		void *data, *d, *s;
 
+		printf("Writing image to png file\n");
+
+		buffer_stride = client->output->width * 4;
+
+		data = xmalloc(buffer_stride * client->output->height);
+		assert(data);
+
+		output_stride = client->output->width * 4;
+		s = screenshot->data;
+		d = data;
+
+		for (i = 0; i < client->output->height; i++) {
+			memcpy(d, s, output_stride);
+			d += buffer_stride;
+			s += output_stride;
+		}
+
+		surface = cairo_image_surface_create_for_data(data,
+							      CAIRO_FORMAT_ARGB32,
+							      client->output->width,
+							      client->output->height,
+							      buffer_stride);
+		cairo_surface_write_to_png(surface, "clientside-screenshot.png");
+		cairo_surface_destroy(surface);
+		free(data);
+	}
+	//assert(match);
+	printf("Test complete\n");
 }
